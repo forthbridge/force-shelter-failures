807be9b6fe45f61f0e37f86853b409fcff59c43f
forthbridge <79264743+forthbridge@users.noreply.github.com>
2023-04-18 15:30:33 +0800

    1.0.2

--- src/Options.cs
+++ src/Options.cs
@@ -20,26 +20,14 @@ namespace ForceShelterFailures
         #endregion
 
         #region Parameters
+
         private readonly float spacing = 20f;
         private readonly float fontHeight = 20f;
-        private readonly int numberOfCheckboxes = 2;
-        private readonly float checkBoxSize = 60.0f;
-        private float CheckBoxWithSpacing => checkBoxSize + 0.25f * spacing;
-        #endregion
-
-        #region Variables
         private Vector2 marginX = new();
         private Vector2 pos = new();
 
+
         private readonly List<float> boxEndPositions = new();
-
-        private readonly List<Configurable<bool>> checkBoxConfigurables = new();
-        private readonly List<OpLabel> checkBoxesTextLabels = new();
-
-        private readonly List<Configurable<string>> comboBoxConfigurables = new();
-        private readonly List<List<ListItem>> comboBoxLists = new();
-        private readonly List<bool> comboBoxAllowEmpty = new();
-        private readonly List<OpLabel> comboBoxesTextLabels = new();
 
         private readonly List<Configurable<int>> sliderConfigurables = new();
         private readonly List<string> sliderMainTextLabels = new();
@@ -47,7 +35,10 @@ namespace ForceShelterFailures
         private readonly List<OpLabel> sliderTextLabelsRight = new();
 
         private readonly List<OpLabel> textLabels = new();
+
         #endregion
+
+
 
         private const int NUMBER_OF_TABS = 1;
 
@@ -66,7 +57,10 @@ namespace ForceShelterFailures
             DrawBox(ref Tabs[tabIndex]);
         }
 
+
+
         #region UI Elements
+
         private void AddTab(ref int tabIndex, string tabName)
         {
             tabIndex++;
@@ -115,105 +109,6 @@ namespace ForceShelterFailures
 
             tab.AddItems(new OpRect(pos, new Vector2(boxWidth, boxEndPositions[lastIndex] - pos.y)));
             boxEndPositions.RemoveAt(lastIndex);
-        }
-
-        private void AddCheckBox(Configurable<bool> configurable, string text)
-        {
-            checkBoxConfigurables.Add(configurable);
-            checkBoxesTextLabels.Add(new OpLabel(new Vector2(), new Vector2(), text, FLabelAlignment.Left));
-        }
-
-        private void DrawCheckBoxes(ref OpTab tab) // changes pos.y but not pos.x
-        {
-            if (checkBoxConfigurables.Count != checkBoxesTextLabels.Count) return;
-
-            float width = marginX.y - marginX.x;
-            float elementWidth = (width - (numberOfCheckboxes - 1) * 0.5f * spacing) / numberOfCheckboxes;
-            pos.y -= checkBoxSize;
-            float _posX = pos.x;
-
-            for (int checkBoxIndex = 0; checkBoxIndex < checkBoxConfigurables.Count; ++checkBoxIndex)
-            {
-                Configurable<bool> configurable = checkBoxConfigurables[checkBoxIndex];
-                OpCheckBox checkBox = new(configurable, new Vector2(_posX, pos.y))
-                {
-                    description = configurable.info?.description ?? ""
-                };
-                tab.AddItems(checkBox);
-                _posX += CheckBoxWithSpacing;
-
-                OpLabel checkBoxLabel = checkBoxesTextLabels[checkBoxIndex];
-                checkBoxLabel.pos = new Vector2(_posX, pos.y + 2f);
-                checkBoxLabel.size = new Vector2(elementWidth - CheckBoxWithSpacing, fontHeight);
-                tab.AddItems(checkBoxLabel);
-
-                if (checkBoxIndex < checkBoxConfigurables.Count - 1)
-                {
-                    if ((checkBoxIndex + 1) % numberOfCheckboxes == 0)
-                    {
-                        AddNewLine();
-                        pos.y -= checkBoxSize;
-                        _posX = pos.x;
-                    }
-                    else
-                    {
-                        _posX += elementWidth - CheckBoxWithSpacing + 0.5f * spacing;
-                    }
-                }
-            }
-
-            checkBoxConfigurables.Clear();
-            checkBoxesTextLabels.Clear();
-        }
-
-        private void AddComboBox(Configurable<string> configurable, List<ListItem> list, string text, bool allowEmpty = false)
-        {
-            OpLabel opLabel = new(new Vector2(), new Vector2(0.0f, fontHeight), text, FLabelAlignment.Center, false);
-            comboBoxesTextLabels.Add(opLabel);
-            comboBoxConfigurables.Add(configurable);
-            comboBoxLists.Add(list);
-            comboBoxAllowEmpty.Add(allowEmpty);
-        }
-
-        private void DrawComboBoxes(ref OpTab tab)
-        {
-            if (comboBoxConfigurables.Count != comboBoxesTextLabels.Count) return;
-            if (comboBoxConfigurables.Count != comboBoxLists.Count) return;
-            if (comboBoxConfigurables.Count != comboBoxAllowEmpty.Count) return;
-
-            float offsetX = (marginX.y - marginX.x) * 0.1f;
-            float width = (marginX.y - marginX.x) * 0.4f;
-
-            for (int comboBoxIndex = 0; comboBoxIndex < comboBoxConfigurables.Count; ++comboBoxIndex)
-            {
-                AddNewLine(1.25f);
-                pos.x += offsetX;
-
-                OpLabel opLabel = comboBoxesTextLabels[comboBoxIndex];
-                opLabel.pos = pos;
-                opLabel.size += new Vector2(width, 2f); // size.y is already set
-                pos.x += width;
-
-                Configurable<string> configurable = comboBoxConfigurables[comboBoxIndex];
-                OpComboBox comboBox = new(configurable, pos, width, comboBoxLists[comboBoxIndex])
-                {
-                    allowEmpty = comboBoxAllowEmpty[comboBoxIndex],
-                    description = configurable.info?.description ?? ""
-                };
-                tab.AddItems(opLabel, comboBox);
-
-                // don't add a new line on the last element
-                if (comboBoxIndex < comboBoxConfigurables.Count - 1)
-                {
-                    AddNewLine();
-                    pos.x = marginX.x;
-                }
-            }
-
-            comboBoxesTextLabels.Clear();
-            comboBoxConfigurables.Clear();
-            comboBoxLists.Clear();
-            comboBoxAllowEmpty.Clear();
         }
 
         private void AddSlider(Configurable<int> configurable, string text, string sliderTextLeft = "", string sliderTextRight = "")
@@ -306,6 +201,7 @@ namespace ForceShelterFailures
             pos.x = marginX.x;
             textLabels.Clear();
         }
+
         #endregion
     }
 }
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,8 +2,6 @@
 using BepInEx.Logging;
 using System.Security.Permissions;
 using System.Security;
-using System.Collections.Generic;
-using UnityEngine;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
 using System;
@@ -21,7 +19,7 @@ namespace ForceShelterFailures
     {
         public static new ManualLogSource Logger { get; private set; } = null!;
 
-        public const string VERSION = "1.0.1";
+        public const string VERSION = "1.0.2";
         public const string MOD_NAME = "Force Shelter Failures";
         public const string MOD_ID = "forceshelterfailures";
         public const string AUTHOR = "forthbridge";
@@ -37,22 +35,26 @@ namespace ForceShelterFailures
 
         private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
         {
-            orig(self);
-
-            if (isInit) return;
-            isInit = true;
-
-            MachineConnector.SetRegisteredOI(MOD_ID, Options.instance);
-
             try
             {
+                if (isInit) return;
+                isInit = true;
+
+                MachineConnector.SetRegisteredOI(MOD_ID, Options.instance);
+
                 IL.RainCycle.ctor += RainCycle_ctor;
             }
             catch (Exception ex)
             {
-                Logger.LogError(ex);
+                Plugin.Logger.LogError(ex);
+            }
+            finally
+            {
+                orig(self);
             }
         }
+
+
 
         private void RainCycle_ctor(ILContext il)
         {
@@ -64,28 +66,27 @@ namespace ForceShelterFailures
                 x => x.MatchLdarg(1),
                 x => x.MatchCallOrCallvirt<World>("get_game"),
                 x => x.MatchCallOrCallvirt<RainWorldGame>("get_GetStorySession"),
-                x => x.MatchLdfld<StoryGameSession>("saveState"),
-                x => x.MatchLdfld<SaveState>("cycleNumber"),
+                x => x.MatchLdfld<StoryGameSession>(nameof(StoryGameSession.saveState)),
+                x => x.MatchLdfld<SaveState>(nameof(SaveState.cycleNumber)),
                 x => x.MatchLdcI4(0),
                 x => x.MatchBgt(out dest));
 
-            c.RemoveRange(7);
             c.Emit(OpCodes.Br, dest);
 
 
+
             c.GotoNext(MoveType.Before,
-                x => x.MatchLdsfld<MoreSlugcats.MoreSlugcats>("cfgDisablePrecycles"));
+                x => x.MatchLdsfld<MoreSlugcats.MoreSlugcats>(nameof(MoreSlugcats.MoreSlugcats.cfgDisablePrecycles)));
 
             c.Index += 2;
-            c.Emit(OpCodes.Pop);
             c.Emit(OpCodes.Ldc_I4_1);
+            c.Emit(OpCodes.Or);
 
 
-
-            c.GotoNext(MoveType.Before,
+            c.GotoNext(MoveType.After,
                 x => x.MatchLdcR4(0.0f));
 
-            c.Remove();
+            c.Emit(OpCodes.Pop);
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Func<RainCycle, float>>(rainCycle => Options.shelterFailureChance.Value - 1);
         }
